f144ffe930b7292530b96b7158afb2968107b980
Lufuno Nemudzivhadi <lufuno.nemudzivhadi26@outlook.com>
2024-05-13 02:44:53 +0200

    update

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
+import { View, Text, FlatList, Image, StyleSheet } from 'react-native';
 import radioStations from '../data/radioStations.json';
 
 const HomeScreen = () => {
@@ -11,12 +11,15 @@ const HomeScreen = () => {
 
   return (
     <View style={styles.container}>
-      <Text>Home Screen</Text>
+      <Text style={styles.title}>Radio Stations</Text>
       <FlatList
         data={stations}
+        numColumns={2} 
         renderItem={({ item }) => (
           <View style={styles.stationItem}>
-            <Text>{item.name}</Text>
+            <Image source={{ uri: item.image }} style={styles.image} />
+            <Text style={styles.name}>{item.name}</Text>
+            <Text style={styles.genre}>{item.genre}</Text>
           </View>
         )}
       />
@@ -27,14 +30,41 @@ const HomeScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'flex-start',
-    justifyContent: 'flex-start',
-    backgroundColor: '#fff',
+    backgroundColor: '#f0f0f0', // Modern background color
     padding: 10,
     paddingTop: 50,
   },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: '#3498DB', // Primary color
+  },
   stationItem: {
     padding: 10,
+    width: '50%',
+    backgroundColor: '#fff', // White background for each item
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 10,
+    elevation: 5,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginBottom: 10,
+  },
+  name: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#2ECC40', // Accent color
+  },
+  genre: {
+    fontSize: 16,
+    color: '#666',
   },
 });
 
