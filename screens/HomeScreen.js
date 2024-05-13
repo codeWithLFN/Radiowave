427d49d6ba9a15cfff55a98862c50daa35af4f4a
Lufuno Nemudzivhadi <lufuno.nemudzivhadi26@outlook.com>
2024-05-13 02:39:51 +0200

    update homescreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
+import radioStations from '../data/radioStations.json';
 
 const HomeScreen = () => {
+  const [stations, setStations] = useState([]);
+
+  useEffect(() => {
+    setStations(radioStations);
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Home Screen</Text>
-      {/* Display list of radio stations here */}
+      <FlatList
+        data={stations}
+        renderItem={({ item }) => (
+          <View style={styles.stationItem}>
+            <Text>{item.name}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 };
@@ -13,13 +27,15 @@ const HomeScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    backgroundColor: '#fff',
+    padding: 10,
+    paddingTop: 50,
   },
-
-
+  stationItem: {
+    padding: 10,
+  },
 });
-
-
 
 export default HomeScreen;
