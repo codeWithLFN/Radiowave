5170c8f6f989dc7d3934cb55cd2c7ccbdf94b195
Lufuno Nemudzivhadi <lufuno.nemudzivhadi26@outlook.com>
2024-05-13 02:28:07 +0200

    update

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 const HomeScreen = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Home Screen</Text>
       {/* Display list of radio stations here */}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+
+});
 
 
 
